7246b2f7ebde04611c7c38bcd60781efac7cf705
Deepak Chaurasiya <86309092+GaryMcKinnoDeepak@users.noreply.github.com>
2023-11-04 18:54:04 +0530

    Button Add

--- assest/js/script.js
+++ assest/js/script.js
@@ -4,28 +4,28 @@ function toggleMenu() {
 }
 
 function showPhoto(selectedPhoto) {
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= 7; i++) {
         document.getElementById(`photo${i}`).style.display = 'none';
     }
     document.getElementById(`photo${selectedPhoto}`).style.display = 'block';
 }
 
 function showImage(selectedImage) {
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= 7; i++) {
         document.getElementById(`image${i}`).style.display = 'none';
     }
     document.getElementById(`image${selectedImage}`).style.display = 'block';
 }
 
 function showCard(selectedCard) {
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= 7; i++) {
         document.getElementById(`Card${i}`).style.display = 'none';
     }
     document.getElementById(`Card${selectedCard}`).style.display = 'block';
 }
 
 function init() {
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= 7; i++) {
         document.getElementById(`radio${i}`).addEventListener('click', function () {
             changePhoto(i);
             showImage(i);
